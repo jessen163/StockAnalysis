145b4f3271b1b8c530f154b37a08e8d7c510b435
jessen <jessen163@gmail.com>
2016-03-31 18:33:06 +0800

    多只股票

--- src/com/ryd/stockanalysis/handle/StTradeThread.java
+++ src/com/ryd/stockanalysis/handle/StTradeThread.java
@@ -50,54 +50,79 @@ public class StTradeThread implements Runnable {
 
 					StQuote sellQuote = Constant.sellList.getLast();
 					StQuote buyQuote = Constant.buyList.getFrist();
-
 					if (sellQuote==null||buyQuote==null) {
 						Thread.sleep(10);
 						continue;
 					}
 
-					if (sellQuote.getStockId().equals(buyQuote.getStockId()) && sellQuote.getAmount()==buyQuote.getAmount()&&Double.doubleToLongBits(sellQuote.getQuotePrice())<=Double.doubleToLongBits(buyQuote.getQuotePrice())) {
+					if (sellQuote.getStockId().equals(buyQuote.getStockId()) && Double.doubleToLongBits(sellQuote.getQuotePrice())<=Double.doubleToLongBits(buyQuote.getQuotePrice())) {
 
 						//添加买入/卖出交易成功后的逻辑
-						//撮合成功，买家增加股票持仓数量，卖家减钱
+						//交易股票
+						StStock sts = Constant.stockTable.get(buyQuote.getStockId());
+						//股票交易数量
+						int tradeStockAmount = 0;
 
-						//交易成功，交易买家持仓增加
-						stPositionServiceI.operateStPosition(buyQuote.getAccountId(), buyQuote.getStockId(),buyQuote.getAmount(), Constant.STOCK_STQUOTE_ACCOUNTMONEY_TYPE_ADD);
+						//买少卖多
+						if (buyQuote.getAmount() < sellQuote.getAmount()) {
 
-						//卖家新增费用
-						double oinmoney = stockAnalysisServiceI.buyOrSellStockMoney(buyQuote.getQuotePrice(),sellQuote.getAmount(),sellQuote.getType());
+							//股票交易数量为买家购买数量
+							tradeStockAmount = buyQuote.getAmount();
 
-						//交易成功，交易卖家资产增加
-						stAccountServiceI.opearteUseMoney(sellQuote.getAccountId(),oinmoney,Constant.STOCK_STQUOTE_ACCOUNTMONEY_TYPE_ADD);
+							stockAnalysisServiceI.trading(buyQuote, sellQuote, tradeStockAmount, sts);
 
-						//添加交易记录
-						StTradeRecord str = new StTradeRecord();
-						str.setId(UUID.randomUUID().toString());
-						str.setBuyerAccountId(buyQuote.getAccountId());
-						str.setSellerAccountId(sellQuote.getAccountId());
-						str.setAmount(sellQuote.getAmount());
-						str.setStockId(sellQuote.getStockId());
-						str.setQuotePrice(buyQuote.getQuotePrice());
-						BigDecimal quotePriceb = new BigDecimal(buyQuote.getQuotePrice());
-						BigDecimal amonutb = new BigDecimal(sellQuote.getAmount());
-						str.setDealMoney(quotePriceb.multiply(amonutb).doubleValue());
-						str.setDateTime(System.currentTimeMillis());
+							//交易成功，交易卖家持仓减少
+							buyQuote.setAmount(buyQuote.getAmount() - tradeStockAmount);
 
-						//股票列表，设置股票编码
-						StStock sts = Constant.stockTable.get(sellQuote.getStockId());
+							//买家移出队列
+							Constant.buyList.removeElement(buyQuote);
 
-						if (sts != null) {
-							str.setStockCode(sts.getStockCode());
+						} else if (buyQuote.getAmount() == sellQuote.getAmount()) {//买卖相等
+
+							tradeStockAmount = buyQuote.getAmount();
+
+							stockAnalysisServiceI.trading(buyQuote,sellQuote,tradeStockAmount,sts);
+
+							//买家卖家移出队列
+							Constant.sellList.removeElement(sellQuote);
+							Constant.buyList.removeElement(buyQuote);
+						}else if(buyQuote.getAmount()>sellQuote.getAmount()){//买多卖少
+
+							//股票交易数量为卖家卖掉数量
+							tradeStockAmount = sellQuote.getAmount();
+
+							stockAnalysisServiceI.trading(buyQuote,sellQuote,tradeStockAmount,sts);
+
+							//交易成功，交易买家报价金额减少
+							Map<String, Object> sellmap = stockAnalysisServiceI.buyOrSellStockMoney(buyQuote.getQuotePrice(), tradeStockAmount, sellQuote.getType());
+							buyQuote.setFrozeMoney(buyQuote.getFrozeMoney() - (double) sellmap.get("rsMoney"));
+							buyQuote.setCommissionFee(buyQuote.getCommissionFee() - (double) sellmap.get("rsMoney"));
+							//卖家移出队列
+							Constant.sellList.removeElement(sellQuote);
 						}
 
-						//交易记录列表
-						stTradeRecordServiceI.addStTradeRecord(str);
-
-						logger.info("交易--买家->" + buyQuote.getAccountId() + "-和-卖家->" + sellQuote.getAccountId() + "--交易成功--"+"--交易股票->"+sts.getStockName() + "--股票编码->"+sts.getStockCode()+"--交易价格->" + buyQuote.getQuotePrice()+ "-交易数量->" + buyQuote.getAmount()+ "-交易总额->" + quotePriceb.multiply(amonutb).doubleValue());
-
-						//移除记录
-						Constant.sellList.removeElement(sellQuote);
-						Constant.buyList.removeElement(buyQuote);
+//					if (!Constant.stTradeQueueMap.isEmpty()) {
+//						for (String s : Constant.stTradeQueueMap.keySet()) {
+//							StTradeQueue stTradeQueueMap = Constant.stTradeQueueMap.get(s);
+//							stTradeQueueMap.buyList.entrySet();
+//							if (stTradeQueueMap.buyList.isEmpty() || stTradeQueueMap.sellList.isEmpty()) continue;
+//							Set<Map.Entry<Long, StQuote>> buyListSet = stTradeQueueMap.buyList.entrySet();
+//							Set<Map.Entry<Long, StQuote>> sellListSet = stTradeQueueMap.sellList.entrySet();
+////							Iterator<Map.Entry<Long, org.jopenexchg.matcher.PriceLeader>> its =  peerPrcLdrSet.iterator();
+//							Iterator<Map.Entry<Long, StQuote>> buyIts = buyListSet.iterator();
+//							Iterator<Map.Entry<Long, StQuote>> sellIts = sellListSet.iterator();
+//							for (Map.Entry<Long, StQuote> buyQuoteEntry: buyListSet) {
+//								for (Map.Entry<Long, StQuote> sellQuoteEntry: sellListSet) {
+//									buyQuote = buyQuoteEntry.getValue();
+//									sellQuote = sellQuoteEntry.getValue();
+//
+//									if (sellQuote.getAmount() == buyQuote.getAmount() && Double.doubleToLongBits(sellQuote.getQuotePrice()) <= Double.doubleToLongBits(buyQuote.getQuotePrice())) {
+//										stTradeQueueMap.buyList.remove(buyQuoteEntry.getKey());
+//										stTradeQueueMap.sellList.remove(sellQuoteEntry.getKey());
+//									}
+//								}
+//							}
+//						}
 					}
 
 
